da4f2136a01b04246db61f4a70776bb6168b8d29
AbdelmalekIla <115870374+AbdelmalekIla@users.noreply.github.com>
2023-12-15 13:44:36 +0100

    Enhance registration to check for existing email, and improve login error handling

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from forms import CreatePostForm, Register
+from forms import CreatePostForm, Register, Login
 # from flask_gravatar import Gravatar
 
 app = Flask(__name__)
@@ -18,6 +18,10 @@ Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
 
 
 ##CONFIGURE TABLES
@@ -33,62 +37,86 @@ class BlogPost(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
-with app.app_context():
-    class User(UserMixin, db.Model):
-        # __tablename__ = "users"
-        id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String(250), unique=True, nullable=False)
-        email = db.Column(db.String(250), unique=True, nullable=False)
-        password = db.Column(db.String(250), unique=True, nullable=False)
-    db.create_all()
 
+class User(UserMixin, db.Model):
+    # __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), unique=True, nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def get_all_posts():
     posts = BlogPost.query.all()
-    return render_template("index.html", all_posts=posts)
+    return render_template("index.html", all_posts=posts, current_user=current_user)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     register_ = Register()
     if register_.validate_on_submit():
-        hash_password = generate_password_hash(request.form.get('password'), method='pbkdf2:sha256', salt_length=8)
-        new_user = User(
-            name=request.form.get('name'),
-            email=request.form.get('email'),
-            password=hash_password
-        )
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for("get_all_posts"))
-    return render_template("register.html", form=register_)
+        user_email = User.query.filter_by(email=request.form.get('email'))
+        if user_email:
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for("login"))
+        else:
+            hash_password = generate_password_hash(request.form.get('password'), method='pbkdf2:sha256', salt_length=8)
+            new_user = User(
+                name=request.form.get('name'),
+                email=request.form.get('email'),
+                password=hash_password
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for("get_all_posts"))
+    return render_template("register.html", form=register_, current_user=current_user)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    login_ = Login()
+    if login_.validate_on_submit():
+        email = login_.email.data
+        password = login_.password.data
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('get_all_posts'))
+    return render_template("login.html", form=login_, current_user=current_user)
 
 
 @app.route('/logout')
 def logout():
+    logout_user()
     return redirect(url_for('get_all_posts'))
 
 
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
     requested_post = BlogPost.query.get(post_id)
-    return render_template("post.html", post=requested_post)
+    return render_template("post.html", post=requested_post, current_user=current_user)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", current_user=current_user)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", current_user=current_user)
 
 
 @app.route("/new-post")
@@ -106,10 +134,11 @@ def add_new_post():
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form)
+    return render_template("make-post.html", form=form, current_user=current_user)
 
 
 @app.route("/edit-post/<int:post_id>")
+@login_required
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
     edit_form = CreatePostForm(
@@ -128,7 +157,7 @@ def edit_post(post_id):
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
 
-    return render_template("make-post.html", form=edit_form)
+    return render_template("make-post.html", form=edit_form, current_user=current_user)
 
 
 @app.route("/delete/<int:post_id>")
